35a40e228c1ece65cd3d2c8cc5554cf923813e73
s-santoro <ssantoro@ntb.ch>
2018-12-03 19:25:38 +0100

    version 1.0

--- main.js
+++ main.js
@@ -1,77 +1,118 @@
-// centre switzerland
-// 46.798449, 8.231879
-
-const accessToken = 'pk.eyJ1Ijoic3NhbnRvcm8iLCJhIjoiY2puazFrcDNzMGFjeTNwbWg2eTc2NG1vaCJ9.dcKsne2XPJhiv7sf2RLIsg';
+const accessToken =
+  "pk.eyJ1Ijoic3NhbnRvcm8iLCJhIjoiY2puazFrcDNzMGFjeTNwbWg2eTc2NG1vaCJ9.dcKsne2XPJhiv7sf2RLIsg";
 let swissGeoJson = null;
-let xml = null;
 let data = null;
+let brands = document.querySelector("#filter").options;
+let markerCluster = L.markerClusterGroup();
+let fuelstations = [];
+let map = L.map("map").setView([46.798449, 8.231879], 8);
 
-// fetch('data_fuel.xml')
-//     // returns promise<String>
-//     .then(response => response.text())
-//     .then(str => (new window.DOMParser()).parseFromString(str, "text/xml"))
-//     .then(data => xml = data);
-
-let map = L.map('map').setView([46.798449, 8.231879], 8);
-
-L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+L.tileLayer(
+  "https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}",
+  {
+    attribution:
+      'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
-    id: 'mapbox.streets',
+    id: "mapbox.streets",
     accessToken: accessToken
-}).addTo(map);
+  }
+).addTo(map);
 
-fetch('swiss_geoJson.json')
-    .then(response => {
-        return response.json()
-    })
-    .then(json => {
-        swissGeoJson = json
-    })
-    .then(() => {
-        L.geoJson(swissGeoJson, {
-            style: function () {
-                return {
-                    fillColor: 'white',
-                    weight: 3,
-                    opacity: 0.6,
-                    color: 'red', //Outline color
-                    fillOpacity: 0
-                }
-            }
-        }).addTo(map);
-    });
+fetch("swiss_geoJson.json")
+  .then(response => {
+    return response.json();
+  })
+  .then(json => {
+    swissGeoJson = json;
+  })
+  .then(() => {
+    L.geoJson(swissGeoJson, {
+      style: function() {
+        return {
+          fillColor: "white",
+          weight: 3,
+          opacity: 0.6,
+          color: "red", //Outline color
+          fillOpacity: 0
+        };
+      }
+    }).addTo(map);
+  });
 
-let key = "_k";
-let value = "_v";
+fetch("data_fuel.json")
+  .then(response => (data = response.json()))
+  .then(json => (data = json))
+  .then(() => {
+    for (let x = 0; x < data.elements.length; x++) {
+      let node = data.elements[x];
+      let lat = node.lat;
+      let lon = node.lon;
+      let name =
+        node.tags.name != undefined ? node.tags.name.toLowerCase() : "no info";
+      let brand =
+        node.tags.brand != undefined
+          ? node.tags.brand.toLowerCase()
+          : "no info";
+      let operator =
+        node.tags.operator != undefined
+          ? node.tags.operator.toLowerCase()
+          : "no info";
+      let popupMsg =
+        "<p>name: " +
+        name +
+        "</p>" +
+        "<p>brand: " +
+        brand +
+        "</p>" +
+        "<p>opeartor: " +
+        operator +
+        "</p>";
 
-fetch('data.json')
-    // returns promise<String>
-    .then(response => data = response.json())
-    .then((json) => data = json)
-    .then(() => {
-        console.log(data.osm.node[450].tag);
-        for (let x = 0; x < data.osm.node.length; x++) {
-            let node = data.osm.node[x];
-            let lat = node._lat;
-            let lon = node._lon;
-            for (let y = 0; y < node.tag.length; y++) {
-                let tag = node.tag[y];
-                let name = null;
-                let brand = null;
-                let street = null;
-                let location = null;
-            }
-            L.marker([lat, lon]).addTo(map);
+      for (i = 0; i < brands.length; i++) {
+        if (
+          brands[i].value === name ||
+          brands[i].value === brand ||
+          brands[i].value === operator
+        ) {
+          let fuelstation = {
+            name: name,
+            brand: brand,
+            operator: operator,
+            lat: lat,
+            lon: lon,
+            popupMsg: popupMsg
+          };
+          fuelstations.push(fuelstation);
+          let marker = L.marker([lat, lon]);
+          marker.bindPopup(popupMsg);
+          markerCluster.addLayer(marker);
         }
-    });
+      }
+    }
+    map.addLayer(markerCluster);
+  })
+  .catch(error => console.log(error));
 
-// let marker = L.marker([46.798449, 8.231879]).addTo(map);
+// Event-listener for select-object
+document.querySelector("#filter").addEventListener("change", filterByBrand);
 
-// let popupMsg =
-//     "<p>name: Bürgi</p>" +
-//     "<p>city: Chur</p>" +
-//     "<p>street: Alte Frauenfelderstrasse</p>" +
-//     "<p>brand: Socar</p>";
-
-// marker.bindPopup(popupMsg);
+// Remove all markers and add only filtered markers
+function filterByBrand() {
+  let brand = this.value;
+  let filteredFuelstations = fuelstations.filter(item => {
+    return (
+      item.name === brand ||
+      item.brand === brand ||
+      item.operator === brand ||
+      "all" === brand
+    );
+  });
+  let filteredMarkers = [];
+  filteredFuelstations.forEach(item => {
+    let marker = L.marker([item.lat, item.lon]);
+    marker.bindPopup(item.popupMsg);
+    filteredMarkers.push(marker);
+  });
+  markerCluster.clearLayers();
+  markerCluster.addLayers(filteredMarkers);
+}
