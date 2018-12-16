b462d61f43ce5f8d78e3d3177a6192b9a56d0ad6
s-santoro <ssantoro@ntb.ch>
2018-12-16 23:25:09 +0100

    added comments

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+// Global variables
+// leaflet access-token
 const accessToken =
   "pk.eyJ1Ijoic3NhbnRvcm8iLCJhIjoiY2puazFrcDNzMGFjeTNwbWg2eTc2NG1vaCJ9.dcKsne2XPJhiv7sf2RLIsg";
 let swissGeoJson = null;
@@ -5,8 +7,10 @@ let data = null;
 let brands = document.querySelector("#filter").options;
 let markerCluster = L.markerClusterGroup();
 let fuelstations = [];
+// set centre of switzerland as initial view
 let map = L.map("map").setView([46.798449, 8.231879], 8);
 
+// create Leaflet-map with streets-tiles
 L.tileLayer(
   "https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}",
   {
@@ -18,6 +22,7 @@ L.tileLayer(
   }
 ).addTo(map);
 
+// fetch local geo-data of border of switzerland
 fetch("swiss_geoJson.json")
   .then(response => {
     return response.json();
@@ -26,6 +31,7 @@ fetch("swiss_geoJson.json")
     swissGeoJson = json;
   })
   .then(() => {
+    // set border-options
     L.geoJson(swissGeoJson, {
       style: function() {
         return {
@@ -40,11 +46,14 @@ fetch("swiss_geoJson.json")
   })
   .catch(error => console.log(error));
 
+// fetch local fuel-station-data
 fetch("data_fuel.json")
   .then(response => (data = response.json()))
   .then(json => (data = json))
   .then(() => {
+    // iterate over each fuel-station-node
     for (let x = 0; x < data.elements.length; x++) {
+      // create initial values for marker
       let node = data.elements[x];
       let lat = node.lat;
       let lon = node.lon;
@@ -58,10 +67,14 @@ fetch("data_fuel.json")
           ? node.tags.operator.toLowerCase()
           : null;
 
+      // get every key-value-pair that exists in the node
+      // and add it to the popup-message
       Object.keys(node.tags).forEach(function(key) {
         popupMsg = popupMsg + getPopUpEntry(key, node.tags[key]);
       });
 
+      // only create markers for fuel-stations
+      // that exist in the filter-options
       for (i = 0; i < brands.length; i++) {
         if (
           brands[i].value === name ||
@@ -76,9 +89,11 @@ fetch("data_fuel.json")
             lon: lon,
             popupMsg: popupMsg
           };
+          // create an array with all fuel-stations for later use
           fuelstations.push(fuelstation);
           let marker = L.marker([lat, lon]);
           marker.bindPopup(popupMsg);
+          // add marker to cluster, improves rapidly performance
           markerCluster.addLayer(marker);
         }
       }
@@ -91,7 +106,7 @@ fetch("data_fuel.json")
 // Event-listener for select-object
 document.querySelector("#filter").addEventListener("change", filterByBrand);
 
-// Remove all markers and add only filtered markers
+// Remove all markers and add only markers select by filter-option
 function filterByBrand() {
   let brand = this.value;
   let filteredFuelstations = fuelstations.filter(item => {
@@ -114,6 +129,7 @@ function filterByBrand() {
     filteredFuelstations.length;
 }
 
+// function for easier popup creation
 function getPopUpEntry(key, value) {
   if (key === "amenity") {
     return "";
